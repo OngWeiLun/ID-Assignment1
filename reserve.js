38461162ddcd92c59d1b66072b5c1cf3a1936034
OngWeiLun <S10222794@connect.np.edu.sg>
2021-12-01 16:44:06 +0800

    cleaned up contact page and menu page

--- reserve.js
+++ reserve.js
@@ -1,9 +1,13 @@
+var res = []
+
 function logfunction(){
-    var info = document.getElementsByTagName("input")
-    for(let i = 0; i < info.length;i++){
-      if (info[i].value !== "Submit"){
-        console.log(info[i].value);
-      }
+  var info = document.getElementsByTagName("input")
+  for(let i = 0; i < info.length;i++){
+    if (info[i].value !== "Submit"){
+      console.log(info[i].value);
+      res.push(info[i].value);
     }
+  }
 }
 
+console.log(res);
